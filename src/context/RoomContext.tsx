ec528df7335cda952b0a667e18a2bf48ec19b58d
qainar <kainaraskarov2002@gmail.com>
2024-02-19 18:54:06 +0600

    add peerjs

--- src/context/RoomContext.tsx
+++ src/context/RoomContext.tsx
@@ -1,6 +1,8 @@
 import SocketIoClient from "socket.io-client";
-import React, {createContext, ReactNode, useEffect} from "react";
+import React, {createContext, ReactNode, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import Peer from "peerjs";
+import {v4 as uuidV4} from 'uuid'
 const SERVER_URL = 'http://localhost:4020'
 
 export const RoomContext = createContext<null | any>(null)
@@ -9,14 +11,19 @@ const ws = SocketIoClient(SERVER_URL)
 
 export const RoomProvider: React.FC<{children: ReactNode}> = ({children}) => {
     const navigate = useNavigate()
+    const [peer, setPeer] = useState<Peer>()
     const enterRoom = async ({room_id}: {room_id: string}) => {
         navigate(`/room/${room_id}`)
     }
     useEffect(() => {
+        const peer_id = uuidV4()
+        const peerInstance = new Peer(peer_id)
+
+        setPeer(peerInstance)
         ws.on('room-created', enterRoom)
     }, [])
     return (
-        <RoomContext.Provider value={{ ws }}>
+        <RoomContext.Provider value={{ ws, peer }}>
             {children}
         </RoomContext.Provider>
     );
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -4,10 +4,12 @@ import {RoomContext} from "../context/RoomContext";
 
 export const Room: React.FC = () => {
     const {id} = useParams()
-    const {ws} = useContext(RoomContext)
+    const {ws, peer} = useContext(RoomContext)
     useEffect(() => {
-        ws.emit('join-room', {room_id: id})
-    }, [])
+        if (peer){
+            ws.emit('join-room', {room_id: id, peer_id: peer._id})
+        }
+    }, [id, peer, ws])
     return (
         <>Room Id ${id}</>
     )
